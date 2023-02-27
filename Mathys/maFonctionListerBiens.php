9f51a326002af603b637cc042c6dd8fca8fce040
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-02-28 00:18:40 +0100

    Add files via upload

---
+++ Mathys/maFonctionListerBiens.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'mesFonctions.php';
+$pdo = connexionBDD();
+?>
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+<?php
+    include_once 'mesFonctions.php';
+    $lesBiens = listerBiens($pdo);
+    foreach ($lesBiens as $unBien){
+        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>';
+    }
+?>
+        
+//<form action="POST" method="">
+//<select name="leBienChoisi">
+
