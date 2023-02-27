faed2aea82433a6cb1aa4f95dc89cd3a27f82861
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-02-27 22:35:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Immobilier</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="pages/acheter/acheter-categories.html">Acheter</a></p>
+                <p><a href="#">Vendre</a></p>
+                <p><a href="#">Louer</a></p>
+                <p><a class="btn-contact" href="pages/contact.html">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+
+    <section class="welcome">
+        <div class="welcome-content">
+            <p>Bienvenue sur le site !
+            </p>
+        </div>
+        <img class="img-accueil" src="img/century21_the_header-image.jpg" alt="">
+    </section>
+
+    <article class="infos">
+        <div class="infos-description">
+            <h1>Le réseau Laforêt, ce sont plus de 700 agences partout en France.</h1>
+            <p>
+                Vous trouverez forcément une agence immobilière Laforêt près de chez vous, 
+                avec des conseillers immobiliers à votre écoute de vos projets : 
+                achat, vente, location, gestion, pour votre maison ou votre appartement.
+            </p>
+            <button onclick="window.location.href = 'pages/acheter/acheter-categories.html';">Trouver un bien</button>
+        </div>
+        <aside class="img-infos">
+            <img src="img/image.png" alt="">
+        </aside>
+    </article>
+
+    <section class="presentation-biens">
+        <h1>Nos derniers biens</h1>
+        <div class="img-biens">
+            <img src="img/maisons/maison1/img1.jpg" alt="">
+            <img src="img/maisons/maison2/img2.jpg" alt="">
+            <img src="img/appart/appart1/img1.jpg" alt="">
+        </div>
+    </section>
+
+    <footer>
+        <div class="column">
+            <h3>Achat</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+        <div class="column">
+            <h3>Vente</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+    </footer>
+</body>
+</html>
