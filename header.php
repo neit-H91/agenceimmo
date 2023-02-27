9249ebae1d443f4aeeb767ee383d7bd3f125abd0
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-02-27 22:38:28 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<head>
+    <link rel="stylesheet" href="styleheader.css">
+</head>
+<html>
+    <header>
+        <nav>
+            <h1><a href="../../index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="../acheter/acheter-categories.html">Acheter</a></p>
+                <p><a class="btn-contact" href="../contact.html">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+</html>
