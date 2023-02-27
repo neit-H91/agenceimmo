238530541b414869cfebbc7351522bb279a87be0
VincentDubois <70756119+duboisVincent@users.noreply.github.com>
2023-02-27 23:37:06 +0100

    Add files via upload

---
+++ VincentFinSprint1/AgenceImmo-main/requête/modele/mesFonctionAccesAuxDonnees.php
@@ -0,0 +1,37 @@
+<?php
+
+function connexionBDD()
+{
+$bdd = 'mysql:host=localhost;dbname=baseprojetimmo';
+$user ='';
+$password = '';
+try {
+   
+    $ObjConnexion=new PDO($bdd,$user,$password,array(
+           PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
+ catch (PDOException $e)
+ {
+     echo $e->getMessage();
+ }
+return $ObjConnexion;
+}
+
+function deconnexionBDD($cnx)
+{
+    $cnx=null;
+}
+
+
+function ChercheBien($pdo,$type,$ville)
+{
+    $sql = " SELECT * FROM Biens INNER JOIN Types ON idType = idTypes WHERE ville = '$ville' AND libelle = '$type'";
+    $test=$pdo->prepare($sql);
+    $test->execute();
+    $lesBiens=$test->fetchAll();
+    return $lesBiens;
+}
+
+
+?>
+
