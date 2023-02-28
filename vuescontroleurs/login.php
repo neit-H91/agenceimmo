a0287ca57f334ed44c58fc5b6906c2bd8d6c5539
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-02-28 13:26:27 +0100

    Update login.php

--- vuescontroleurs/login.php
+++ vuescontroleurs/login.php
@@ -1,8 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="stylelogin.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="../css/stylelogin.css" media="screen" type="text/css" />
     </head>
     <body>
         <header>
