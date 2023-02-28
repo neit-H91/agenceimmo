b9579e1f931c15aefadf74e144456a903b08772f
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-02-28 13:28:59 +0100

    Update menuagent.php

--- vuescontroleurs/menuagent.php
+++ vuescontroleurs/menuagent.php
@@ -1,7 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <!-- importer le fichier de style -->
-    <link rel="stylesheet" href="stylemenuagent.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="../css/stylemenuagent.css" media="screen" type="text/css" />
 </head>
 <body>
     <header>
@@ -13,8 +12,8 @@
         <div id="contenu">
             <h1>Bienvenu sur votre espace :</h1>
             <section class="acheter-links">
-                <button onclick="window.location.href = 'acheter-appartements.html';">Modifier un bien</button>
-                <button onclick="window.location.href = 'acheter-appartements.html';">Ajouter un bien</button>
+                <button onclick="window.location.href = #;">Modifier un bien</button>
+                <button onclick="window.location.href = #;">Ajouter un bien</button>
             </section>
         </div>
     </div>
