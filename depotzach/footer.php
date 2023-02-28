c2cb788eb3e17a4175437507222adbdff3847162
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-02-28 02:07:43 +0100

    Add files via upload

---
+++ depotzach/footer.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="stylefooter.css">
+</head>
+<html>
+    <footer>
+        <div class="column">
+            <h3>Navigation</h3>
+            <p><a href="../../index.html">Acceuil</a></p>
+            <p><a class="btn-contact" href="../contact.html">Contact</a></p>
+            <p><a class="btn-connexion" href="">Se connecter</a></p>
+        </div>
+    </footer>
+</html>
+
+
