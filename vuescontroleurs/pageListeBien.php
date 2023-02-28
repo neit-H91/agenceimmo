e596a060649bce71f701728b70caabfd80d99a6a
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-02-28 14:21:31 +0100

    Create pageListeBien.php

---
+++ vuescontroleurs/pageListeBien.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/stylemenuagent.css" media="screen" type="text/css" />
+</head>
+<body>
+    <header>
+        <?php
+          include('header.php');
+        ?>
+    </header>
+  
+    include_once '../autres/listerBiens.php
+  
+    <footer>
+        <?php
+          include('footer.php');
+        ?> 
+    </footer>
+</body>
+</html>
