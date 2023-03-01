8decbf3e4c6d2d8bd2796bb829c7e3d56c75f159
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 23:37:43 +0100

    Update testNewTypes.php

--- tests/testNewTypes.php
+++ tests/testNewTypes.php
@@ -6,7 +6,7 @@
  */
 
 //on insère le fichier qui contient les fonctions
-include_once '../modeles/mesFonctionsBDD.php';
+include_once '../modeles/mesFonctionsAccesBDD.php';
 
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
