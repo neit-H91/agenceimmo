26c4ebec4dbb45c535bd1ab69eb90a1df260acc4
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:41:19 +0100

    Create acheter-terrain.php

---
+++ vuescontroleurs/acheter/acheter-terrain.php
@@ -0,0 +1,206 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acheter</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="../../index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="acheter-categories.php">Acheter</a></p>
+                <p><a href="#">Vendre</a></p>
+                <p><a href="#">Louer</a></p>
+                <p><a class="btn-contact" href="../contact.php">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+
+    <section class="liens-biens">
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain1.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain1/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain1.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 2</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain2.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain2/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain2.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 3</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain3.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terains/terrain/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain3.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 4</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain4.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain4/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain4.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 5</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain5.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain5/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain5.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 6</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain6.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain6.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 7</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain7.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain7/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain7.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 8</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain8.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain8/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain8.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Terrain 9</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../terrains/terrain9.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/terrains/terrain9/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../terrains/terrain9.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="column">
+            <h3>Achat</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+        <div class="column">
+            <h3>Vente</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+    </footer>
+</body>
+</html>
