e1c389feffbaaf55249b3d5d94c643196c7bef40
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:54:31 +0100

    Update formRechercheBien.php

--- vuescontroleurs/formRechercheBien.php
+++ vuescontroleurs/formRechercheBien.php
@@ -1,9 +1,20 @@
 <html>
+	
 <head>
-	<title>Formulaire de recherche</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Recherche</title>
+    <link rel="stylesheet" href="../css/style-rechercheBien.css">
 </head>
+	
 <body>
 
+	<?php
+		include('agenceimmo/in/entete.inc');
+	?>
+	
+	
 	<h1>Formulaire de recherche</h1>
 
 	<form method="post" action="../autres/rechercheBien.php">
@@ -15,6 +26,11 @@
 
 		<input type="submit" value="Rechercher">
 	</form>
+	
+    <?php
+        include('footer.php');
+    ?> 
 
 </body>
+	
 </html>
