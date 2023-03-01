b2dde9adbc21659485df31666d4a0d636a62a0a8
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:39:12 +0100

    Create acheter-maisons.php

---
+++ vuescontroleurs/acheter/acheter-maisons.php
@@ -0,0 +1,206 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acheter</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="../../index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="acheter-categories.php">Acheter</a></p>
+                <p><a href="#">Vendre</a></p>
+                <p><a href="#">Louer</a></p>
+                <p><a class="btn-contact" href="../contact.php">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+
+    <section class="liens-biens">
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison1.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison1/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison1.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 2</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison2.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison2/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison2.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 3</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison3.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison3/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison3.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 4</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison4.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison4/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison4.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 5</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison5.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison5/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison5.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 6</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison6.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison6/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison6.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 7</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison7.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison7/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison7.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 8</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison8.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison8/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison8.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Maison 9</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../maisons/maison9.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/maisons/maison9/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../maisons/maison9.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="column">
+            <h3>Achat</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+        <div class="column">
+            <h3>Vente</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+    </footer>
+</body>
+</html>
