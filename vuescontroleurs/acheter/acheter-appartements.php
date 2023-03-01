9839662f0cc3e25b34c5ef95171709dcd4b43fa7
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:31:27 +0100

    Create acheter-appartements.php

---
+++ vuescontroleurs/acheter/acheter-appartements.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acheter</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="../../index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="acheter-categories.php">Acheter</a></p>
+                <p><a href="#">Vendre</a></p>
+                <p><a href="#">Louer</a></p>
+                <p><a class="btn-contact" href="../contact.php">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+
+    <section class="liens-biens">
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart1.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart1/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart1.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 2</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart2.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart2/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart2.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 3</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart3.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart3/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart3.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 4</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart4.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart4/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart4.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 5</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart5.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart5/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart5.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 6</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart6.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart6/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart6.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 7</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart7.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart7/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart7.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 8</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart8.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart8/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart8.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Appartement 9</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../appartements/appart9.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/appart/appart9/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../appartements/appart9.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        
+    </section>
+
+    <footer>
+        <div class="column">
+            <h3>Achat</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+        <div class="column">
+            <h3>Vente</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+    </footer>
+</body>
+</html>
