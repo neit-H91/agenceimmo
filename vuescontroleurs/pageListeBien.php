f915c8c7a1dfc62acfcf88a0f3cc7621cfaef642
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:59:31 +0100

    Update pageListeBien.php

--- vuescontroleurs/pageListeBien.php
+++ vuescontroleurs/pageListeBien.php
@@ -3,18 +3,18 @@
     <link rel="stylesheet" href="../css/style-listerBiens.css" media="screen" type="text/css" />
 </head>
 <body>
-    <header>
+
         <?php
           include('header.php');
         ?>
-    </header>
+
   
     include_once '../autres/listerBiens.php
   
-    <footer>
+
         <?php
           include('footer.php');
         ?> 
-    </footer>
+
 </body>
 </html>
