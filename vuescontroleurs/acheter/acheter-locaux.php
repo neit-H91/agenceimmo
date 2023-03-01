07a7c2cf0ab0ab99e8dd8782fb0b6328131e6cc2
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 21:37:13 +0100

    Create acheter-locaux.php

---
+++ vuescontroleurs/acheter/acheter-locaux.php
@@ -0,0 +1,207 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acheter</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="../../index.html">Immobilier</a></h1>
+            <div class="right">
+                <p><a href="acheter-categories.php">Acheter</a></p>
+                <p><a href="#">Vendre</a></p>
+                <p><a href="#">Louer</a></p>
+                <p><a class="btn-contact" href="../contact.php">Contact</a></p>
+            </div>
+        </nav>
+    </header>
+
+    <section class="liens-biens">
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local1.php""><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local1/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local1.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 2</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local2.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local2/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local2.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 3</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local3.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local3/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local3.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 4</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local4.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local4/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local4.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 5</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local5.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local5/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local5.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 6</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local6.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local6/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local6.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 7</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local7.php"><><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local7/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local7.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 8</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../Locaux/local8.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/locaux8/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local8.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        <div class="biens">
+            <div class="biens-description">
+                <h1>Local 9</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Proin consequat tincidunt metus, efficitur mattis nisl. 
+                    Sed risus orci, aliquam et tortor viverra, ultricies porttitor est. 
+                    Morbi vel diam in arcu sagittis feugiat eget ac diam. Nulla euismod suscipit elementum. 
+                    Nam sed lacus eget lorem maximus condimentum ut quis odio.
+                </p>
+                <a href="../locaux/local9.php"><button>En savoir plus</button></a>
+            </div>
+            <div class="biens-img">
+                <img src="../../img/locaux/local9/img1.jpg" alt="">
+            </div>
+            <div class="bien-btn">
+                <a href="../locaux/local9.php"><button>En savoir plus</button></a>
+            </div>
+        </div>
+        
+    </section>
+
+    <footer>
+        <div class="column">
+            <h3>Achat</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+        <div class="column">
+            <h3>Vente</h3>
+            <p>Maison</p>
+            <p>Apartement</p>
+            <p>Locaux</p>
+            <p>Terrains</p>
+            <p>Immeubles</p>
+        </div>
+    </footer>
+</body>
+</html>
