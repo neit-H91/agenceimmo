6f7ee88d3c924a4dffaa8a1c32e6fc2515485fe1
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-01 23:34:57 +0100

    Update testRechercheBien.php

--- tests/testRechercheBien.php
+++ tests/testRechercheBien.php
@@ -1,3 +1,5 @@
+<?php
+
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
@@ -9,8 +11,8 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
 
-$type = 'maison'
-$ville = 'roubaix'
+$type = 'maison';
+$ville = 'roubaix';
   
 ChercheBien($lePdo,$type,$ville);
 
