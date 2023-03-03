5ab880c0d8e4e8fcc9a71247988ceddc27552e68
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-03 21:03:12 +0100

    Add files via upload

---
+++ tests/testVerification.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+//on insère le fichier qui contient les fonctions
+include_once '../modeles/mesFonctionsAccesBDD.php';
+
+//appel de la fonction qui permet de se connecter à la base de données
+$lePdo = connexionBDD();
+
+$texte = recuperation($lePdo,"mathyslaouadi59960@gmail.com");
+
+//var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
+var_dump($lePdo);
