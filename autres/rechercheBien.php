7a8d05d956bd0fad843cc610652d8354cf7a7053
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-05 19:03:59 +0100

    Update rechercheBien.php

--- autres/rechercheBien.php
+++ autres/rechercheBien.php
@@ -5,27 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Acheter</title>
-        <link rel="stylesheet" href="../css/style-rechercheBien.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="../css/style-accueil.css">
     </head>
     
     <body>
-        
-            <?php
-            include('../inc/entete.inc');
-            ?>
-        
-        <div id="Aligne-choix">
-            <form action="../modeles/mesFonctionsAccesBDD.php" method="POST">
 
-                <h1> Recherche de Biens</h1>
-
-                
-
-        </div>
-
-            <?php
-            include('../inc/piedDePage.inc');
-            ?> 
+                <?php
+                    include_once '../modeles/mesFonctionsAccesBDD.php';
+                    $pdo = connexionBDD();
+                    $V=$_POST['parametre1'];
+                    $T=$_POST['parametre2'];
+                    $desBiens = ChercheBien($pdo, $T, $V);
+                    foreach ($desBiens as $unBien){
+                        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>';
+                    }
+                ?>  
 
     </body>
 </html>
