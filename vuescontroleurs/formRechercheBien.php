0191af691c21d4fb6fd97f3cfe4d30586b51be0f
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-05 18:30:50 +0100

    Update formRechercheBien.php

--- vuescontroleurs/formRechercheBien.php
+++ vuescontroleurs/formRechercheBien.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire de Recherche</title>
-    <link rel="stylesheet" href="../css/style-rechercheBien.css">
+    <link rel="stylesheet" href="/agenceimmo/css/style-rechercheBien.css">
 </head>
 	
 <body>
@@ -14,18 +14,19 @@
 		include('../inc/entete.inc');
 	?>
 	
-	
-	<h1>Formulaire de recherche</h1>
+	<div id=Aligne-choix>
+		<h1>Formulaire de recherche</h1>
+		<form method="post" action="../autres/rechercheBien.php">
+			<label for="parametre1">Paramètre 1 :</label>
+			<input type="text" id="parametre1" name="parametre1"><br><br>
 
-	<form method="post" action="../autres/rechercheBien.php">
-		<label for="parametre1">Paramètre 1 :</label>
-		<input type="text" id="parametre1" name="parametre1"><br><br>
-
-		<label for="parametre2">Paramètre 2 :</label>
-		<input type="text" id="parametre2" name="parametre2"><br><br>
-
-		<input type="submit" value="Rechercher">
-	</form>
+			<label for="parametre2">Paramètre 2 :</label>
+			<input type="text" id="parametre2" name="parametre2"><br><br>
+			<div id=Boite-bouton>
+				<input type="submit" value="Rechercher">
+			</div>
+		</form>
+	</div>
 	
     <?php
         include('../inc/piedDePage.inc');
