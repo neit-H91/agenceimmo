8088bf221b38e9517c0c2a2a6de1f88f4c8ed24d
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-05 22:39:50 +0100

    Update listerBiens.php

--- autres/listerBiens.php
+++ autres/listerBiens.php
@@ -3,8 +3,6 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 $pdo = connexionBDD();
 ?>
 
-    <div id=listerBiens>
-
 <?php
     include_once '../modeles/mesFonctionsAccesBDD.php';
     $lesBiens = listerBiens($pdo);
@@ -12,5 +10,5 @@ $pdo = connexionBDD();
         echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>';
     }
 ?>     
-    </div>   
+
 
