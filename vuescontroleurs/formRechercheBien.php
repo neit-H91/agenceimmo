29cf236d9a0f9e32f4a11f44a7d775ee1d3a08bb
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-06 14:31:35 +0100

    Update formRechercheBien.php

--- vuescontroleurs/formRechercheBien.php
+++ vuescontroleurs/formRechercheBien.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire de Recherche</title>
-    <link rel="stylesheet" href="/agenceimmo/css/style-rechercheBien.css">
+    <link rel="stylesheet" href="../css/style-rechercheBien.css">
 </head>
 	
 <body>
