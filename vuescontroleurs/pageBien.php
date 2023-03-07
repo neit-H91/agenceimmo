98d74c97bd585f31f412661e9397058dda6429d0
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-07 14:01:11 +0100

    Create pageBien.php

---
+++ vuescontroleurs/pageBien.php
@@ -0,0 +1,84 @@
+<?php
+    include('../inc/entete.inc');
+?>
+
+
+<div class="Page">
+    <div class="contenu">
+        <div class="presentation">
+            <div class="texte">
+                <div class="titre">
+                    POST
+                </div>
+                <div class="location">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2838/2838912.png" alt="">
+                    POST à POST (POST)
+                </div>
+                <div class="prix">
+                    POST €
+                </div>
+            </div>
+            <div class="photos">
+                <div class="image-principale">
+                    <img src="../../img/terrains/terrain1/img1.jpg" alt="">
+                </div>
+                <div class="image-secondaires">
+                    <div class="row1"><img src="../../img/terrains/terrain1/img2.jpg" alt="">
+                    <img src="../../img/terrains/terrain1/img3.jpg" alt="">
+                    <img src="../../img/terrains/terrain1/img4.jpg" alt=""></div>
+                    <div class="row2">
+                        <img src="../../img/terrains/terrain1/img5.jpg" alt="">
+                    <img src="../../img/terrains/terrain1/img6.jpg" alt="">
+                    <img src="../../img/terrains/terrain1/img7.jpg" alt="">
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="dessous">
+            <div class="description">
+                POST<br>
+                <!--<div class="caracs">
+                <div class="un"><img src="../../img/pictos/chambres picto.png" alt="">
+                <h4>3 chambres</h4>
+                </div>
+                <div class="deux">
+                    <img src="../../img/pictos/sdb picto.png" alt="">
+                    <h4>2 salle de bain</h4>
+                </div>
+                <div class="trois">
+                    <img src="../../img/pictos/surface picto.png" alt="">
+                    <h4>200m² avec jardin</h4>
+                </div>    
+                </div>-->
+            </div>
+            <div class="Avis">
+                <h3>Avis de l'agent</h3><br>
+                <p>POST</p>
+            </div>
+            <div class="bilan-energetique">
+                <h3>Bilan energetique</h3><br><br><br>
+                <div class="consomation">
+                    <div class="Chauffage">
+                        <p>Diagnostique de performances énergétique (DPE)</p><br>
+                        POST
+                        <img src="../../img/consos/perfEnergie/conso B.png" alt="">
+                    </div>
+                    <div class="Effet-de-Serre">
+                        <p>Indice d'emission à effet de serre (GES)</p><br>
+                        POST
+                        <img src="../../img/consos/effetSerre/effet de serre C.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="emplacement">
+                <h3>Emplacement</h3><br>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20228.25697971604!2d3.21862995!3d50.67294705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c327cc74383821%3A0x40af13e81646260!2s59390%20Lys-lez-Lannoy!5e0!3m2!1sfr!2sfr!4v1673963531105!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+</div>
+</div>
+
+<?php
+    include('../inc/piedDePage.inc');
+?>
