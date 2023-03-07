371366f820eb94cd66bd33e93dc127097d33e574
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-07 14:25:11 +0100

    Update verification.php

--- autres/verification.php
+++ autres/verification.php
@@ -8,7 +8,9 @@ $login=$_POST['username'];
 $mdp=$_POST['password'];
 //verification de la correspondance des hash de mdp
 $hash=recuperation($pdo,$login);
-if (md5($mdp) === $hash) {
+if ((hash('sha256',$mdp)) === $hash) {
+    session_start();
+    $_SESSION['login']=$login;
     header('Location: ../vuescontroleurs/menuagent.php');
 } else {
     header('Location: ../vuescontroleurs/erreur.php');
