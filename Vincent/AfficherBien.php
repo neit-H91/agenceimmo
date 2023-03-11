e278463eaf5ba870762fe2353b68b68ef70c3312
VincentDubois <70756119+duboisVincent@users.noreply.github.com>
2023-03-11 20:32:35 +0100

    Add files via upload

---
+++ Vincent/AfficherBien.php
@@ -0,0 +1,17 @@
+<html>
+<?php
+            include('../inc/entete.inc');
+            ?>
+    {!--Code pour afficher les informations du bien --!}
+    {!--A faire:    
+                1- code php pour l'affichage des biens ( Requête Sql )
+                2- faire une phrase générique pour afficher les infos les biens
+                3-
+        
+ 
+        --!}
+    
+<?php
+            include('../inc/piedDePage.inc');
+            ?>
+</html>
