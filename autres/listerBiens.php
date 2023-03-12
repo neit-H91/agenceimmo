76338a85f6fa1d5eb2a9e080402f3f5222e749e3
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-13 00:29:50 +0100

    Update listerBiens.php

--- autres/listerBiens.php
+++ autres/listerBiens.php
@@ -8,6 +8,11 @@ $pdo = connexionBDD();
     $lesBiens = listerBiens($pdo);
     foreach ($lesBiens as $unBien){
         echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>';
+?>
+
+    <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>       
+
+<?php
     }
 ?>     
 
