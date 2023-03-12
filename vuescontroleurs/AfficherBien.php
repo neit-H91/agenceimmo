98889949abab1c629586b39a08385ca2c179acaa
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-12 22:09:18 +0100

    Update AfficherBien.php

--- vuescontroleurs/AfficherBien.php
+++ vuescontroleurs/AfficherBien.php
@@ -2,14 +2,14 @@
 <?php
             include('../inc/entete.inc');
             ?>
-    {!--Code pour afficher les informations du bien --!}
-    {!--A faire:    
-                1- code php pour l'affichage des biens ( Requête Sql )
-                2- faire une phrase générique pour afficher les infos les biens
-                3-
-        
- 
-        --!}
+            <!--Code pour afficher les informations du bien id - 
+                code php pour l'affichage des biens ( Requête Sql )
+                2- faire une phrase générique pour afficher les infos les biens -->
+
+    <?php
+        echo $_GET['id'];
+        include_once '../autres/RechercheInformation.php'; 
+    ?>
     
 <?php
             include('../inc/piedDePage.inc');
