9a9dd8c0ca55450615f5f31846c427eefa27f1a1
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-12 22:06:58 +0100

    Create RechercheInformation.php

---
+++ autres/RechercheInformation.php
@@ -0,0 +1,6 @@
+<?php
+include_once '../modeles/mesFonctionsAccesBDD.php';
+    $pdo = connexionBDD();
+    $IdBien = $_GET['id'];
+    $desInformations = AfficheInformation($pdo, $IdBien);
+        echo '<h1>' . desInformations['titre'] .  '</h1>'; // récupéré de la fonction AfficheInformation
