e39ead44ad0af5eab22b79b099039b0f754728f4
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-13 00:25:51 +0100

    Update rechercheBien.php

--- autres/rechercheBien.php
+++ autres/rechercheBien.php
@@ -1,15 +1,14 @@
-<html>
-               
     <?php             
         include_once '../modeles/mesFonctionsAccesBDD.php';               
         $pdo = connexionBDD();           
         $V=$_POST['parametre1'];   
         $T=$_POST['parametre2'];           
-        $desBiens = ChercheBien($pdo, $T, $V);              
+        $desBiens = ChercheBien($pdo, $T, $V);
         foreach ($desBiens as $unBien){          
             echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle']
-            .' au prix de '.$unBien['prix'].'€ à '.$unBien['ville']
-            . ' d\'id: ' . $unBien['idBien'] .'.<a href="../vuescontroleurs/AfficherBien.php?id='.$unBien['idBien'].'"> Cliquez-ici pour le voir </a></option>';             
-        }          
+            .' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>'
+    ?>
+            <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>
+    <?php                 
+        }         
     ?>  
-</html>
