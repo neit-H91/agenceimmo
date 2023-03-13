8d05863fcf205e06855adeeeb209bb57f6d69905
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-13 13:51:39 +0100

    Update pageBien.php

--- pageBien.php
+++ pageBien.php
@@ -82,3 +82,13 @@
 <?php
     include('../inc/piedDePage.inc');
 ?>
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page bien</title>
+    <link rel="stylesheet" href="../css/style-biens.css">
+</head>
