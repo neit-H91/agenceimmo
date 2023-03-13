efc764763b8e9f1c27019717c1553096fffbbf87
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-13 06:24:29 +0100

    Update formAjoutBien2.php

--- vuescontroleurs/formAjoutBien2.php
+++ vuescontroleurs/formAjoutBien2.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+// On teste si la variable de session existe et contient une valeur
+if(empty($_SESSION['login'])) 
+{
+  // Si inexistante ou nulle, on redirige vers le formulaire de login
+  header('Location: login.php');
+  exit();
+}
+?>
 <html>
 	
 <head>
