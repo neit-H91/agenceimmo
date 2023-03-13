5471fa47ffbf266449557da764b9e48f30110401
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-13 23:19:25 +0100

    Update rechercheBien.php

--- autres/rechercheBien.php
+++ autres/rechercheBien.php
@@ -1,10 +1,12 @@
     <?php             
         include_once '../modeles/mesFonctionsAccesBDD.php';               
         $pdo = connexionBDD();           
-        $V=$_POST['parametre1'];   
-        $T=$_POST['parametre2'];           
-        $desBiens = ChercheBien($pdo, $T, $V);
-        foreach ($desBiens as $unBien){          
+        $V=$_POST['ville'];   
+        $T=$_POST['type'];
+        $P=$_POST['prix'];
+        $J=$_POST['jardin'];
+        $biens = ChercheBien($pdo, $T, $V,$P,$J);
+        foreach ($biens as $unBien){          
             echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle']
             .' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>'
     ?>
