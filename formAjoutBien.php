0b67d4f0f770e857967cebfa210c310dd40624d7
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-13 15:08:20 +0100

    Update formAjoutBien.php

--- formAjoutBien.php
+++ formAjoutBien.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+// On teste si la variable de session existe et contient une valeur
+if(empty($_SESSION['login'])) 
+{
+  // Si inexistante ou nulle, on redirige vers le formulaire de login
+  header('Location: login.php');
+  exit();
+}
+?>
 <html>
 	
 <head>
@@ -16,80 +26,137 @@
 
 	<div id="page">
             <div id="contenu">
-                <h1>Formulaire de recherche</h1>
+                <h1>Formulaire d'ajout de biens</h1>
+		 <p>Bienvenue sur votre formulaire d'ajout de biens, ce formulaire vous permets d'ajouter un bien à une base de donées contenant tous les biens,
+                 une fois ajouté une page se créera automatiquement pour le bien, afin d'être sûr des valeurs à entrer vous pouvez survolez ou cliquez sur les 
+                 bulles informations "?" à droite des zones de saisie</p>
                 <form action="../autres/ajouterBiens.php" method="POST">
-                    <div id="formulaire">
-                        <div id="blocs1">    
+                    <div id="formulaire">           
                             <div class="bloc">
-                                <label for="parametre1">Saisissez la description du bien :</label>
-                                <p></p>
-                                <input type="text" id="parametre1" name="description" required>
+                                <div class="colonne1">
+                                    <label for="parametre1">Saisissez la description du bien :</label>
+                                    </br>  
+                                    <input type="text" id="parametre1" name="description" required>
+                                </div>    
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez la description du bien">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre2">Saisissez le prix de bien :</label>      
-                                <p></p>                      
-                                <input type="text" id="parametre2" name="prix" required>
+                                <div class="colonne1">
+                                    <label for="parametre2">Saisissez le prix de bien :</label>      
+                                    </br>                    
+                                    <input type="text" id="parametre2" name="prix" required>
+                                </div>        
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez un nombre">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre3">Saisissez l'adresse :</label>
-                                <p></p>
-                                <input type="text" id="parametre3" name="adresse" required>
+                                <div class="colonne1">
+                                    <label for="parametre3">Saisissez l'adresse :</label>
+                                    </br>
+                                    <input type="text" id="parametre3" name="adresse" required>
+                                </div>    
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez l'adresse, numero et rue">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre4">Saisissez la ville du bien :</label>    
-                                <p></p>                        
-                                <input type="text" id="parametre4" name="ville" required>
+                                <div class="colonne1">
+                                    <label for="parametre4">Saisissez la ville du bien :</label>    
+                                    </br>                        
+                                    <input type="text" id="parametre4" name="ville" required>
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez la ville du bien en toute lettre">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre5">Saisissez le Code Postal de la ville du bien:</label>
-                                <p></p>
-                                <input type="text" id="parametre5" name="codepostal" required>
+                                <div class="colonne1">
+                                    <label for="parametre5">Saisissez le Code Postal de la ville du bien:</label>
+                                    </br>
+                                    <input type="text" id="parametre5" name="codepostal" required>
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez un nombre">?</a>
+                                </div>
                             </div>
-                        </div>
-                        <div id="blocs2">
+                               
                             <div class="bloc">
-                                <label for="parametre6">Saisissez la surface du bien :</label>
-                                <p></p>                          
-                                <input type="text" id="parametre6" name="surfacebien" required>
+                                <div class="colonne1">
+                                    <label for="parametre6">Saisissez la surface du bien :</label>
+                                    </br>
+                                    <input type="text" id="parametre6" name="surfacebien" required>
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez la surface du bien, ou du terrain sous forme de nombre en m2">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre7">Saisissez la surface du jardin :</label>
-                                <p></P>
-                                <input type="text" id="parametre7" name="surfacejardin">
+                                <div class="colonne1">
+                                    <label for="parametre7">Saisissez la surface du jardin :</label>
+                                    </br>  
+                                    <input type="text" id="parametre7" name="surfacejardin">
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez la surface du bien, ou du terrain sous forme de nombre en m2</br>Cette valeur peut être nulle, auquel cas ne -saisissez rien">?</a>
+                                </div>
                             </div>
+
                             <div class="bloc">
-                                <label for="parametre8">Saisissez le nombre de pièces du bien :</label>   
-                                <p></p>                         
-                                <input type="text" id="parametre8" name="nbpiece" >
+                                <div class="colonne1">
+                                    <label for="parametre8">Saisissez le nombre de pièces du bien :</label> 
+                                    </br> 
+                                    <input type="text" id="parametre8" name="nbpiece" >
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez le nombre de pièces du bien</br>Cette valeur peut être nulle, auquel cas ne -saisissez rien">?</a>
+                                </div>                       
                             </div>
-                            <div class="bloc">
-                                <label for="parametre9">Saisissez l'identifiant du type de bien :</label>
-                                <p></p>
-                                <input type="text" id="parametre9" name="idtype" required>
+
+                            <div class="blocfin">
+                                <div class="colonne1">
+                                    <label for="parametre9">Choisissez le type de bien :</label>
+                                    </br>
+                                    <select name="formAjoutTypeBien" id="formAjoutTypeBien">
+                                        <option value="maison">Maison</option>
+                                        <option value="appartement">Appartement</option>
+                                        <option value="terrain">Terrain</option>
+                                        <option value="local">Local</option>
+                                        <option value="immeuble">Immeuble</option>   
+                                    </select>                                
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="-Saisissez 1 pour une maison \n -Saisissez 2 pour un appartement</br>-Saisissez 3 pour un terrain</br>-Saisissez 4 pour un local</br>-Saisissez 5 pour un immeuble</br>">?</a>
+                                </div>
                             </div>
-                            <div class="bloc">
-                                <label for="parametre10">Saisissez le titre d'affichage du bien :</label>      
-                                <p></p>                      
-                                <input type="text" id="parametre10" name="titre" required>
-                            </div>
-                        </div>
+
+                            <div class="blocfin">
+                                <div class="colonne1">
+                                    <label for="parametre10">Saisissez le titre d'affichage du bien :</label>      
+                                    </br>                   
+                                    <input type="text" id="parametre10" name="titre" required>
+                                </div>
+                                <div class="pictos">
+                                    <a class="picto-item" id="pdt" href="#" aria-label="Saisissez le titre, c'est à dire la phrase qui décrira le bien">?</a>
+                                </div>
+                            </div>     
                     </div>
+
+
                     <div id="formulairelogin">
-                        <input type="submit" value="Rechercher">
+                        <input type="submit" value="Ajouter le bien à la base de données">
                     </div>
                 </form>
 
-                <div id="recherche">
-                <?php
-                if (isset($_POST["parametre1"]) && isset($_POST["parametre2"]) && isset($_POST["parametre3"]) && isset($_POST["parametre4"]) && isset($_POST["parametre5"]) && isset($_POST["parametre6"]) && isset($_POST["parametre9"]) && isset($_POST["parametre10"])){                    
-                    include('../inc/validationAjoutBien.inc');
-                }   
-    	        ?>
-                </div>
-
             </div>               
         </div>
-
 
     	<?php
         include('../inc/piedDePage.inc');
@@ -98,3 +165,4 @@
 </body>
 	
 </html>
+
