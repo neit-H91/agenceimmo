83ee7373d177d5bb3e819910dc9f2605c94542ec
VincentDubois <70756119+duboisVincent@users.noreply.github.com>
2023-03-14 14:53:16 +0100

    Add files via upload

---
+++ tests/testRechercheInformation.php
@@ -0,0 +1,12 @@
+<?php
+
+//on insère le fichier qui contient les fonctions
+include_once '../modeles/mesFonctionsAccesBDD.php';
+
+//appel de la fonction qui permet de se connecter à la base de données
+$lePdo = connexionBDD();
+
+AfficheInformation($lePdo,1);
+
+//var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
+var_dump($lePdo);
