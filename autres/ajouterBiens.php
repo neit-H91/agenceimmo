31379f935692f954b04a28cdbec8ecac41dd1a7e
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-14 14:27:09 +0100

    Update ajouterBiens.php

--- autres/ajouterBiens.php
+++ autres/ajouterBiens.php
@@ -17,3 +17,4 @@
 
     ajouterBien($pdo, $_POST['description'],  $_POST['prix'], $_POST['adresse'], $_POST['ville'], $_POST['codepostal'], $_POST['surfacebien'] , $SJ , $NBP , $_POST['formAjoutTypeBien'], $_POST['titre']);
 
+    header('Location: ../vuescontroleurs/menuagent.php');
