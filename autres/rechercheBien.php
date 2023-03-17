37611da26703383182a13891d4102eebadd896ba
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-17 21:43:24 +0100

    Update rechercheBien.php

--- autres/rechercheBien.php
+++ autres/rechercheBien.php
@@ -5,7 +5,9 @@
         $T=$_POST['type'];
         $P=$_POST['prix'];
         $J=$_POST['jardin'];
-        $biens = ChercheBien($pdo, $T, $V,$P,$J);
+        $SM=$_POST['surfaceMini'];
+        $PM=$_POST['piecesMini'];
+        $biens = ChercheBien($pdo, $T, $V,$P,$J,$SM,$PM);
         foreach ($biens as $unBien){          
             echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle']
             .' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>'
@@ -13,4 +15,4 @@
             <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>
     <?php                 
         }         
-    ?>  
+    ?> 
