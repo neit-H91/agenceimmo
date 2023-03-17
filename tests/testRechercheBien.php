f575560fa0d4b299a92e9c5c90d9548379735f1e
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-17 21:43:57 +0100

    Update testRechercheBien.php

--- tests/testRechercheBien.php
+++ tests/testRechercheBien.php
@@ -11,10 +11,14 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
 
-$type = 'maison';
-$ville = 'roubaix';
-  
-ChercheBien($lePdo,$type,$ville);
+$type = 'appartement';
+$ville = 'Tourcoing';
+$prix = 600000 ;
+$jardin = 'oui' ;
+$surfaceMini = 40 ;
+$piecesMini = 4 ;
+
+$test = ChercheBien($lePdo,$type, $ville,$prix,$jardin,$surfaceMini,$piecesMini);
 
 //var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
-var_dump($lePdo);
+var_dump($test);
