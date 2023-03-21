78ec2dcb09a0c371b0abf3a63aa5a70df1b8a2e2
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-21 21:04:49 +0100

    Update testListerBiens.php

--- tests/testListerBiens.php
+++ tests/testListerBiens.php
@@ -11,9 +11,9 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
 
-listerBiens($lePdo);
+$test = listerBiens($lePdo);
 
 //var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
-var_dump($lePdo);
+var_dump($test);
 
  
