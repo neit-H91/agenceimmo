5ea999177ffb01021202d504ea3db5733ff2f34b
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-21 21:05:36 +0100

    Update testRechercheInformation.php

--- tests/testRechercheInformation.php
+++ tests/testRechercheInformation.php
@@ -6,7 +6,7 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
 
-AfficheInformation($lePdo,1);
+$test = AfficheInformation($lePdo,1);
 
 //var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
-var_dump($lePdo);
+var_dump($test);
