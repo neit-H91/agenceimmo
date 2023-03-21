6ce32b158dc5170333dd4fe0920fc183ae3c7fa3
VincentDubois <70756119+duboisVincent@users.noreply.github.com>
2023-03-21 14:41:35 +0100

    Update RechercheInformation.php

--- autres/RechercheInformation.php
+++ autres/RechercheInformation.php
@@ -3,5 +3,4 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
     $pdo = connexionBDD();
     $IdBien = $_GET['id'];
     $desInformations = AfficheInformation($pdo, $IdBien);
-        echo '<h1>' . $desInformations['titre'] .  '</h1>'; // récupéré de la fonction AfficheInformation
 ?>
