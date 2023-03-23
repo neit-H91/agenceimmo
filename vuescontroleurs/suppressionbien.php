c40d28f3089b7faabe13c59c8c3c8c72db8ae93f
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-23 11:59:17 +0100

    Add files via upload

---
+++ vuescontroleurs/suppressionbien.php
@@ -0,0 +1,61 @@
+<html>
+
+<?php
+session_start();
+// On teste si la variable de session existe et contient une valeur
+if(empty($_SESSION['login'])) 
+{
+  // Si inexistante ou nulle, on redirige vers le formulaire de login
+  header('Location: login.php');
+  exit();
+}
+include_once '../autres/recuperationNom.php';
+?>
+  
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/stylemenuagent.css" media="screen" type="text/css" />
+</head>
+<body>
+    
+    <?php
+        include('../inc/entete.inc');
+    ?>
+    
+    <div id="page">
+        <div id="contenu">
+            <h1>Supprimer un bien : </h1>
+            <?php
+                    include_once '../modeles/mesFonctionsAccesBDD.php';
+                    $bdd = connexionBDD();
+                    ?>
+            <form action="">
+                <div>
+                    <label for="id">choisir l'id du bien a supprimer</label>
+                    <select name="idbien" id="idbien">
+                        <option value=""></option>
+                        <?php
+                                    $reponse = $bdd->query('SELECT distinct idBien FROM biens');
+                                    while ($donnes = $reponse->fetch()) {
+                                        ?>
+                                        <option value="<?php echo $donnes['idBien']; ?>"><?php echo $donnes['idBien']; ?></option>;
+                                        <?php
+                                    }
+                                    $reponse->closeCursor();
+                                    ?>
+                    </select>
+                </div>
+            </form>
+
+            <section class="acheter-links">
+              <button onclick="window.location.href = 'menuagent.php';">Retour menu</button>
+            </section>
+        </div>
+    </div>
+        
+    <?php
+        include('../inc/piedDePage.inc');
+    ?> 
+        
+</body>
+</html>
