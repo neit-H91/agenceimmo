58f441a244ac64b06cfd83443ed3669f36b7bdac
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-27 22:23:07 +0200

    Update listerBiens.php

--- autres/listerBiens.php
+++ autres/listerBiens.php
@@ -10,18 +10,18 @@ $pdo = connexionBDD();
         echo '<center><div class="liste"><table>';
         echo '<tr>';
         echo '<th class="thliste">Référence</th>';
+        echo '<th class="thliste">Ville</th>';
         echo '<th class="thliste">Type</th>';
         echo '<th class="thliste">Prix</th>';
-        echo '<th class="thliste">Ville</th>';
         echo '<th class="thliste">Voir le bien</th>';
         echo '</tr>';
    
         foreach ($lesBiens as $unBien){
             echo '<tr>';
             echo '<td class="tdliste">' . $unBien['idBien'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
             echo '<td class="tdliste">' . $unBien['libelle'] . '</td>';
             echo '<td class="tdliste">' . $unBien['prix'] . '</td>';
-            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
             echo '<td class="tdliste">' ?><a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a></td>
             <?php
             echo '</tr>';
