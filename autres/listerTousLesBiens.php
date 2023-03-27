261dab9d6f74dc071ebb53f74f8e430f2b060293
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-27 22:23:29 +0200

    Update listerTousLesBiens.php

--- autres/listerTousLesBiens.php
+++ autres/listerTousLesBiens.php
@@ -11,18 +11,18 @@ $pdo = connexionBDD();
         echo '<center><div class="liste"><table>';
         echo '<tr>';
         echo '<th class="thliste">Référence</th>';
+        echo '<th class="thliste">Ville</th>';
         echo '<th class="thliste">Type</th>';
         echo '<th class="thliste">Prix</th>';
-        echo '<th class="thliste">Ville</th>';
         echo '<th class="thliste">Voir le bien</th>';
         echo '</tr>';
    
         while($unBien = $reponse->fetch()){ // Renvoit les valeurs de la bdd
             echo '<tr>';
             echo '<td class="tdliste">' . $unBien['idBien'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
             echo '<td class="tdliste">' . $unBien['libelle'] . '</td>';
             echo '<td class="tdliste">' . $unBien['prix'] . '</td>';
-            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
             echo '<td class="tdliste">' ?><a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a></td>
             <?php
             echo '</tr>';
