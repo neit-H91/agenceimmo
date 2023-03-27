0c626cc0450604ae7017bb6a3ee9c52a4ed76953
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-27 22:03:33 +0200

    Update listerBiens.php

--- autres/listerBiens.php
+++ autres/listerBiens.php
@@ -6,14 +6,28 @@ $pdo = connexionBDD();
 <?php
     include_once '../modeles/mesFonctionsAccesBDD.php';
     $lesBiens = listerBiens($pdo,$_POST['ID'],$_POST['VILLE'],$_POST['TYPE'],$_POST['PRIX']);
-    foreach ($lesBiens as $unBien){
-        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].' de référence '.$unBien['idBien'].'</option>';
-?>
 
-    <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>       
-
-<?php
-    }
-?>        
+        echo '<center><div class="liste"><table>';
+        echo '<tr>';
+        echo '<th class="thliste">Référence</th>';
+        echo '<th class="thliste">Type</th>';
+        echo '<th class="thliste">Prix</th>';
+        echo '<th class="thliste">Ville</th>';
+        echo '<th class="thliste">Voir le bien</th>';
+        echo '</tr>';
+   
+        foreach ($lesBiens as $unBien){
+            echo '<tr>';
+            echo '<td class="tdliste">' . $unBien['idBien'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['libelle'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['prix'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
+            echo '<td class="tdliste">' ?><a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a></td>
+            <?php
+            echo '</tr>';
+        }
+        echo '</table></div></center>';
+        //$pdo = null;
+?>     
 
 
