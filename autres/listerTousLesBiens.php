3fac9edc2d15c4fd2870b2396f4a2872bb8aeda1
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-27 22:04:27 +0200

    Update listerTousLesBiens.php

--- autres/listerTousLesBiens.php
+++ autres/listerTousLesBiens.php
@@ -5,13 +5,28 @@ $pdo = connexionBDD();
 
 <?php
     include_once '../modeles/mesFonctionsAccesBDD.php';
-    $lesBiens = listerBiens($pdo,'','','','');
-    foreach ($lesBiens as $unBien){
-        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].' de référence '.$unBien['idBien'].'</option>';
+    //$lesBiens = listerBiens($pdo);
+    $reponse = $pdo->query('SELECT idBien,libelle,prix,ville FROM biens INNER JOIN types ON idType = idTypes');
+
+        echo '<center><div class="liste"><table>';
+        echo '<tr>';
+        echo '<th class="thliste">Référence</th>';
+        echo '<th class="thliste">Type</th>';
+        echo '<th class="thliste">Prix</th>';
+        echo '<th class="thliste">Ville</th>';
+        echo '<th class="thliste">Voir le bien</th>';
+        echo '</tr>';
+   
+        while($unBien = $reponse->fetch()){ // Renvoit les valeurs de la bdd
+            echo '<tr>';
+            echo '<td class="tdliste">' . $unBien['idBien'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['libelle'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['prix'] . '</td>';
+            echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
+            echo '<td class="tdliste">' ?><a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a></td>
+            <?php
+            echo '</tr>';
+        }
+        echo '</table></div></center>';
+        //$pdo = null;
 ?>
-
-    <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>       
-
-<?php
-    }
-?>     
