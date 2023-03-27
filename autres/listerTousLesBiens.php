ef626decc9c9a4a70fd57028503eff9297232625
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-03-27 13:59:58 +0200

    Update listerTousLesBiens.php

--- autres/listerTousLesBiens.php
+++ autres/listerTousLesBiens.php
@@ -7,7 +7,7 @@ $pdo = connexionBDD();
     include_once '../modeles/mesFonctionsAccesBDD.php';
     $lesBiens = listerBiens($pdo,'','','','');
     foreach ($lesBiens as $unBien){
-        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].'</option>';
+        echo '<option value="'.'">'.'- '.$unBien['titre'].', un(e) '.$unBien['libelle'].' au prix de '.$unBien['prix'].'€ à '.$unBien['ville'].' de référence '.$unBien['idBien'].'</option>';
 ?>
 
     <a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a>       
