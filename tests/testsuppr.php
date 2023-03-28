dfcea5cde68ec558c737c32fe789093003e12f1f
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-28 14:07:14 +0200

    Add files via upload

---
+++ tests/testsuppr.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+//on insère le fichier qui contient les fonctions
+include_once '../modeles/mesFonctionsAccesBDD.php';
+
+//appel de la fonction qui permet de se connecter à la base de données
+$lePdo = connexionBDD();
+
+$test = supprBien($lePdo,4);
+
+//var_dump permet d'afficher le contenu d'un objet. Utilisable uniquement lors de test de validation
+var_dump($test);
