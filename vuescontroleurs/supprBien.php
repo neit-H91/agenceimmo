1ef14d91a0b55f7cba669b0e45ad5e3d808347b0
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-03-28 14:29:01 +0200

    Delete supprBien.php

--- vuescontroleurs/supprBien.php
+++
@@ -1,9 +0,0 @@
-<?php
-//on insère le fichier qui contient les fonctions
-include_once '../modeles/mesFonctionsAccesBDD.php';
-//connexion à la bdd
-$pdo = connexionBDD();
-//recuperation des info rentré
-$id=$_POST['idbien'];
-//execution de la requete
-$supp=supprBien($pdo,$id);
