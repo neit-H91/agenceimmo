8156b4488738bc383715848055092c1940a10d48
MathysLa <122391064+MathysLa@users.noreply.github.com>
2023-04-04 23:56:48 +0200

    Update testGetAllVille.php

--- tests/testGetAllVille.php
+++ tests/testGetAllVille.php
@@ -6,6 +6,6 @@ include_once '../modeles/mesFonctionsAccesBDD.php';
 //appel de la fonction qui permet de se connecter à la base de données
 $lePdo = connexionBDD();
 
-getAllVille($lePdo);
+$test = getAllVille($lePdo);
 
-var_dump($lePdo);
+var_dump($test);
