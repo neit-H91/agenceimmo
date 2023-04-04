b23ecb6930f08260073c491a5dd47eca88fcf733
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-04-04 14:51:29 +0200

    Update rechercheBien.php

--- autres/rechercheBien.php
+++ autres/rechercheBien.php
@@ -1,21 +1,20 @@
     <?php             
         include_once '../modeles/mesFonctionsAccesBDD.php';               
         $pdo = connexionBDD();           
-        $V=$_POST['ville'];   
+        /*$V=$_POST['ville'];   */
         $T=$_POST['type'];
-        $P=$_POST['prixMin'];
+        $TR=$_POST['prix'];
         $J=$_POST['jardin'];
         $SM=$_POST['surfaceMini'];
         $PM=$_POST['piecesMini'];
-        $D=$_POST['prixMax'];
         $ID=$_POST['idChoix'];
-        $biens = ChercheBien($pdo, $T, $V,$P,$D,$J,$SM,$PM,$ID);
+        $biens = ChercheBien($pdo, $T, /*$V,*/$TR,$J,$SM,$PM,$ID);
         //$oui=$biens->rowCount();
         if($biens!=null){
             echo '<center><div class="liste"><table>';
             echo '<tr>';
             echo '<th class="thliste">Référence</th>';
-            echo '<th class="thliste">Ville</th>';
+            /*echo '<th class="thliste">Ville</th>';*/
             echo '<th class="thliste">Type</th>';
             echo '<th class="thliste">Prix</th>';
             echo '<th class="thliste">Voir le bien</th>';
@@ -24,7 +23,7 @@
             foreach ($biens as $unBien){          
                 echo '<tr>';
                 echo '<td class="tdliste">' . $unBien['idBien'] . '</td>';
-                echo '<td class="tdliste">' . $unBien['ville'] . '</td>';
+                /*echo '<td class="tdliste">' . $unBien['ville'] . '</td>';*/
                 echo '<td class="tdliste">' . $unBien['libelle'] . '</td>';
                 echo '<td class="tdliste">' . $unBien['prix'] .' €'. '</td>';
                 echo '<td class="tdliste">' ?><a href="AfficherBien.php?id=<?php echo $unBien['idBien'] ?>"> Cliquez-ici pour le voir </a></td>
@@ -36,3 +35,4 @@
         }
         //$pdo = null;
 ?>
+
