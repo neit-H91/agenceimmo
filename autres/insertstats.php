35b67327113c6d93cd72920cbfed06edd154b528
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-04-11 14:33:19 +0200

    Update insertstats.php

--- autres/insertstats.php
+++ autres/insertstats.php
@@ -15,19 +15,12 @@ if(!empty($_POST["surfaceMini"])){
 if(!empty($_POST["prix"])){
     $tranche=$_POST["prix"];
 }
-var_dump($ville);
-var_dump($surface);
-var_dump($tranche);
 
 $pdo = connexionBDD();
 $insert=$pdo->prepare("insert into recherche values (NULL,:d,:idv,:idt,:surfBien)");
 $bv1=$insert->bindValue(':d',$date,PDO::PARAM_STR);
-echo $bv1;
 $bv2=$insert->bindValue(':idv',$ville,PDO::PARAM_INT);
-echo $bv2;
 $bv3=$insert->bindValue(':idt',$tranche,PDO::PARAM_INT);
-echo $bv3;
 $bv4=$insert->bindValue(':surfBien',$surface,PDO::PARAM_INT);
-echo $bv4;
 $insert->execute();
 ?>
