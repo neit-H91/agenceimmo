a972e28114cbe0b8c1aa1df0d4e50ac8b820e1f6
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-04-11 14:32:33 +0200

    Update teststats.php

--- tests/teststats.php
+++ tests/teststats.php
@@ -1,29 +1,21 @@
 <?php
-date_default_timezone_set('Europe/Paris');
 include_once '../modeles/mesFonctionsAccesBDD.php';
-$date=date('Y-m-d');
-$ville="Roubaix";
+
+$ville=5;
+$surface=800;
+$tranche=NULL;
+$date =date('Y-m-d');
 
 $pdo = connexionBDD();
-$requete=$pdo->prepare("select idVille from ville where libelle = :v");
-$requete->bindValue(':v',$ville,PDO::PARAM_STR);
-$requete->execute();
-$idVille=$requete->fetchAll(PDO::FETCH_ASSOC);
-var_dump($idVille);
-$idTranche=NULL;
-
-$insert=$pdo->prepare("insert into recherche values (NULL,:d,:idv,:idt)");
+$insert=$pdo->prepare("insert into recherche values (NULL,:d,:idv,:idt,:surfBien)");
 $bv1=$insert->bindValue(':d',$date,PDO::PARAM_STR);
-var_dump($bv1);
-$bv2=$insert->bindValue(':idv',$idVille,PDO::PARAM_INT);
-var_dump($bv2);
-$bv3=$insert->bindValue(':idt',$idTranche,PDO::PARAM_INT);
-var_dump($bv3);
-$insert->execute();
+echo $bv1;
+$bv2=$insert->bindValue(':idv',$ville,PDO::PARAM_INT);
+echo $bv2;
+$bv3=$insert->bindValue(':idt',$tranche,PDO::PARAM_INT);
+echo $bv3;
+$bv4=$insert->bindValue(':surfBien',$surface,PDO::PARAM_INT);
+echo $bv4;
 
-
-/* cmmd qui permet de savoir quelle ville a été cherché cb de fois
-select idVille, COUNT(idVille) from recherche GROUP BY idVille; */
-
-/* cmmd qui permet de savoir quelle tranche a été cherché cb de fois
-select idTranche, COUNT(idTranche) from recherche group by(idTranche); */
+var_dump($insert->execute());
+?>
