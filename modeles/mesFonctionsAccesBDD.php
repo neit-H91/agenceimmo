b89387363dffa546766f216abcb711140dd01da8
neit-H91 <122883717+neit-H91@users.noreply.github.com>
2023-05-02 13:28:17 +0200

    Update mesFonctionsAccesBDD.php

--- modeles/mesFonctionsAccesBDD.php
+++ modeles/mesFonctionsAccesBDD.php
@@ -365,3 +365,85 @@ if($prixrentré > prixMax){
    } 
 }
 */
+
+function statsVille(){
+    echo '<center><div class="liste"><table>';
+echo '<tr>';
+echo '<th class="thliste">Ville</th>';
+echo '<th class="thliste">Occurence</th>';
+echo '</tr>';
+$sql = "SELECT libelleVille FROM `ville`;";
+$test=$pdo->prepare($sql);
+$test->execute();
+$result=$test->fetchAll(PDO::FETCH_COLUMN, 0);
+$sql="select count(recherche.idVille) from recherche join ville on recherche.idVille=ville.idVille where ville.libelleVille=:v and recherche.dateRecherche BETWEEN :dD and :dF;";
+$cmmd=$pdo->prepare($sql);
+$cmmd->bindValue(':dD',$dateDebut);
+$cmmd->bindValue(':dF',$dateFin);
+foreach ($result as $element) {
+        $cmmd->bindValue(':v',$element);
+        $cmmd->execute();
+        $count=$cmmd->fetch();
+        echo '<tr>';
+        echo '<td class="tdliste">' . $element. '</td>';
+        echo '<td class="tdliste">' . $count[0]. '</td>';
+        echo '</tr>';
+               
+}
+
+echo '</table></div></center>';
+}
+function statsTranche(){
+    echo '<center><div class="liste"><table>';
+
+echo '<tr>';
+echo '<th class="thliste">Tranche</th>';
+echo '<th class="thliste">Occurence</th>';
+echo '</tr>';
+$sql="SELECT prixMin,prixMax FROM `tranche`;";
+$test=$pdo->prepare($sql);
+$test->execute();
+$result=$test->fetchAll();
+$sql="select count(recherche.idTranche) from recherche join tranche on recherche.idTranche=tranche.idTranche where tranche.prixMin = :pMin and tranche.prixMax = :pMax and recherche.dateRecherche between :dD and :dF;";
+$test=$pdo->prepare($sql);
+$test->bindValue(':dD',$dateDebut);
+$test->bindValue(':dF',$dateFin);
+foreach($result as $element){
+    $min=$element[0];
+    $max=$element[1];
+    $test->bindValue(':pMin',$min);
+    $test->bindValue(':pMax',$max);
+    $test->execute();
+    $count=$test->fetch();
+    echo '<tr>';
+    echo '<td class="tdliste">' . $min.'-'.$max. '</td>';
+    echo '<td class="tdliste">' .$count[0]. '</td>';
+    echo '</tr>';
+}
+
+echo '</table></div></center>';
+}
+function statsSurface(){
+    echo '<center><div class="liste"><table>';
+
+echo '<tr>';
+echo '<th class="thliste">Surface</th>';
+echo '<th class="thliste">Occurence</th>';
+echo '</tr>';
+$sql="SELECT surface,count(surface) FROM `recherche` where surface is not null and recherche.dateRecherche between :dD and :dF group by surface;";
+$test=$pdo->prepare($sql);
+$test->bindValue(':dD',$dateDebut);
+$test->bindValue(':dF',$dateFin);
+$test->execute();
+$result=$test->fetchAll();
+foreach($result as $element){
+    $surface=$element[0];
+    $count=$element[1];
+    echo '<tr>';
+    echo '<td class="tdliste">' . $surface.'</td>';
+    echo '<td class="tdliste">' . $count. '</td>';
+    echo '</tr>';
+}
+
+echo '</table></div></center>';
+}
